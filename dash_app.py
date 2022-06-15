daf9b757f765afa6c814816c00188bb270eb07a6
adityapise <aditya.pise1302@gmail.com>
2022-06-14 20:55:59 -0700

    added title to the line graph, and changed id of the graph

--- dash_app.py
+++ dash_app.py
@@ -4,10 +4,10 @@ import pandas as pd
 app = Dash(__name__)
 df = pd.read_csv("final_data.csv")
 df = df.sort_values(by="Date")
-fig = px.line(df, x="Date", y="Sales")
+fig = px.line(df, x="Date", y="Sales", title="Sales of Pink Morsel")
 app.layout = html.Div(children=[html.H1(children='Sales of Pink Morsel'),
                                 dcc.Graph(
-                                    id='example-graph',
+                                    id='visualization',
                                     figure=fig
                                 )])
 
